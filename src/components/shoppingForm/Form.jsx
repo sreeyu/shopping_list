78052379fd03ccebf7ff5f2498677c52b1a04d43
sreey <sreelakshmi12898@gmail.com>
2023-07-31 20:51:59 +0530

    Accept user input

--- src/components/shoppingForm/Form.jsx
+++ src/components/shoppingForm/Form.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Form.module.css';
 
 function Form(){
+
+    const [item, setItem] = useState('');
+
+    const getItem = (event) => {
+        setItem(event.target.value);
+    }
+
+    const formSubmit = (event) => {
+        event.preventDefault();
+        console.log(item);
+        setItem('');
+    }
+    
     return(
         <div className={styles["shopping-form"]}>
-            <form >
-            <label htmlFor="">Enter your shopping items here</label>
-            <input type="text" />
+            <form onSubmit={formSubmit} >
+            <label id="input-list">Enter your shopping items here</label>
+            <input type="text" id="input-list" value={item} onChange={getItem} />
             <button>Add item</button>
         </form>
         </div>
